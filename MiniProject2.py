e6e6a0c2e66bffa4b1f5b2c4206e8568dbc53ba4
Nicklas <nicklasmundt@gmail.com>
2024-03-12 14:19:50 +0100

    X and y

--- MiniProject2.py
+++ MiniProject2.py
@@ -5,3 +5,9 @@ import pandas as pd
 current_dir = os.path.dirname(os.path.abspath(__file__))
 data = pd.read_csv(os.path.join(current_dir, "./data/mushrooms.csv"))
 col_names = data.columns
+# print(data.shape)
+# print(col_names)
+
+X = data.drop('class', axis=1)
+y = data['class']
+
